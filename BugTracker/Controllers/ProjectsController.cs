672d15fcd2ad81e78c23a81c34d5f9ebc5706101
Brandon M. Beck <55066809+bbeck7412@users.noreply.github.com>
2019-12-06 15:43:23 -0500

    update

--- BugTracker/Controllers/ProjectsController.cs
+++ BugTracker/Controllers/ProjectsController.cs
@@ -24,6 +24,7 @@ namespace BugTracker.Controllers
         // GET: Projects
         
         public ActionResult Index()
+
         {
             if (User.Identity.IsAuthenticated)
             {
