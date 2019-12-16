3982fac4cb63773c5cf4c5fc7e368c9b5248932d
Brandon M. Beck <55066809+bbeck7412@users.noreply.github.com>
2019-12-16 15:43:49 -0500

    update

---
+++ BugTracker/Models/pmDropdownViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class pmDropdownViewModel
+    {
+        public Project Project { get; set; }
+        public ApplicationUser ProjectManager { get; set; }
+
+    }
+}
